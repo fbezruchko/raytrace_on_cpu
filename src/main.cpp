ffdb26922def88fbd8125df9dcd5447239d78ea8
Fedor Bezruchko <fbezruchko@gmail.com>
2021-03-27 01:15:10 +1000

    Add image generation with stb library

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,24 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STBIW_WINDOWS_UTF8
+#include <stb_image_write.h>
 
 int main() {
-    int W = 720;
-    int H = 480;
+    int W = 1366;
+    int H = 768;
 
-    cout << "P3\n" << W << " " << H << "\n255\n";
-    vector < vector <int> > pixels(W*H);
+    std::vector <uint8_t> pixels(W*H*3);
     for (int y = 0; y < H; y++) {
+        int h = W*y;
         for (int x = 0; x < W; x++) {
-            int w = W*y;
-            pixels[w + x].resize(3);
-            pixels[w + x][0] = int(255 * float(x) / float(W));
-            pixels[w + x][1] = int(255 * float(y) / float(H));
-            pixels[w + x][2] = int(255 * 0.8);
+            int p = (h+x)*3;
+            pixels[p] = uint8_t(255 * float(x) / float(W));
+            pixels[p + 1] = uint8_t(255 * float(y) / float(H));
+            pixels[p + 2] = uint8_t(255 * 0.8);
         }
     }
 
-    for (int y = H-1; y >= 0; y--) {
-        for (int x = 0; x < W; x++) {
-            int w = W*y;
-            cout << pixels[w + x][0] << " " << pixels[w + x][1] << " " << pixels[w + x][2] << "\n";
-        }
-    }
-
+    stbi_write_png("image_stb.png", W, H, 3, pixels.data(), W*3);
 }
